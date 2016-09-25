362bbd971ac8607a8eb71b3b0b5bb175236f3626
rajjmaurya <rajjmaurya@gmail.com>
2016-09-25 19:55:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 //change the text of
 var element=document.getElementById("main-text");
 element.innerHTML="new value";
-var img=document.getElementById("img");
+var img=document.getElementById("madi");
 img.onclick =function(){
     
     img.style.margineLeft='100px';
